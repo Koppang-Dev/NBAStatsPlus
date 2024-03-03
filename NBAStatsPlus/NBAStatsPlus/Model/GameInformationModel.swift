ec3da9e835a00730e47327611c1a667529648f64
Riley <koppangdev@gmail.com>
2024-03-03 16:32:19 -0700

    Created Service Folder

---
+++ NBAStatsPlus/NBAStatsPlus/Model/GameInformationModel.swift
@@ -0,0 +1,48 @@
+//
+//  GameInformationModel.swift
+//  NBAStatsPlus
+//
+//  Created by Riley on 2024-03-03.
+//
+
+import Foundation
+
+// Parent struct that contains the game/team data and meta data
+struct GameListResponse: Decodable {
+    let data: [GameInformation]
+    let meta: MetaInformation?
+}
+
+// Object that contains specific information about the Meta Data
+struct MetaInformation: Decodable {
+    let current_page: Int
+    let next_page: Int?
+    let per_page: Int
+}
+
+// Contains specific game information
+struct GameInformation: Decodable {
+    let id: Int
+    let date: String
+    let home_team_score: Int
+    let visitor_team_score: Int
+    let season: Int
+    let period: Int
+    let status: String
+    let time: String? // Optional becomes some games start later
+    let postseason: Bool
+    let home_team: TeamInformation
+    let visitor_team: TeamInformation
+}
+
+// Object that contains information about the specific team
+struct TeamInformation: Decodable {
+    let id: Int
+    let abbreviation: String
+    let city: String
+    let conference: String
+    let division: String
+    let full_name: String
+    let name: String
+}
+
---
+++ NBAStatsPlus/NBAStatsPlus/Service/APIKit/APICaller.swift
@@ -0,0 +1,23 @@
+//
+//  APICaller.swift
+//  APIKit
+//
+//  Created by Riley on 2024-03-02.
+//
+
+import Foundation
+
+public class APICaller {
+    
+    // Creating Singleton Instance
+    public static let shared = APICaller()
+    private init() {}
+    
+    public func fetchGameInformation() {
+        
+    }
+
+    
+    
+    
+}
