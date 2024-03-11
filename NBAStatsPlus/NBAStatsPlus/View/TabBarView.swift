57d7313af7adcad0d5f9952eb3be1e90404f15e5
Riley <koppangdev@gmail.com>
2024-03-11 15:05:06 -0600

    Added Reels to the tab bar

--- NBAStatsPlus/NBAStatsPlus/View/TabBarView.swift
+++ NBAStatsPlus/NBAStatsPlus/View/TabBarView.swift
@@ -16,6 +16,7 @@ class TabBarView: UITabBarController {
     //MARK: Properties
     let vc1 = GameListView()
     let vc2 = StandingView()
+    let vc3 = ReelsView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,19 +29,17 @@ class TabBarView: UITabBarController {
         // Setting the tab images
         vc1.tabBarItem.image = UIImage(systemName: "figure.basketball")
         vc2.tabBarItem.image = UIImage(systemName: "list.clipboard")
+        vc3.tabBarItem.image = UIImage(systemName: "play.fill")
         
         // Setting title for each of the view controllers
         vc1.tabBarItem.title = "Games"
         vc2.tabBarItem.title = "Standings"
+        vc3.tabBarItem.title = "Reels"
         
         tabBar.tintColor = .black
         
-        // Setting the view controllers as UINavigationViewController
-        let nav1 = UINavigationController(rootViewController: vc1)
-        let nav2 = UINavigationController(rootViewController: vc2)
-        
         // Adding the view controllers in the NavigatioViewController
-        setViewControllers([nav1, nav2], animated: false)
+        setViewControllers([vc1, vc2, vc3], animated: false)
     }
 }
 
