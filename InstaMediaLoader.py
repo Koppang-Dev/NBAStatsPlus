bf85a3afae2e437581b3d0ced1f0cd6911762337
Riley <koppangdev@gmail.com>
2024-03-13 13:42:45 -0600

    Fixed script issues

--- InstaMediaLoader.py
+++ InstaMediaLoader.py
@@ -1,33 +1,38 @@
-
-# Importing Instaloader
 import instaloader
 from instaloader import Instaloader, Profile
-L = instaloader.Instaloader() # Initalizing instance
-import schedule
-import time
+import os
 
+# Initialize Instaloader
+L = instaloader.Instaloader()
 
-def download_posts():
-    # Selected Profile to import from
-    PROFILE = "nba"
-    profile = Profile.from_username(L.context, PROFILE) # Telling instaloader this is the profile we want data from
-    profilePosts = profile.get_posts()
+# Selected Profile to import from
+PROFILE = "nba"
+profile = Profile.from_username(L.context, PROFILE) # Telling instaloader this is the profile we want data from
+profilePosts = profile.get_posts()
 
-    #Counter to track the number of downloaded posts
-    downloaded_count = 0
+# Specify the directory where you want to save the media files
+save_dir = "/Users/ri/Documents/NBAStatsPlus/NBAStatsPlus/NBAStatsPlus/DownloadedMedia"  # Update this with your desired directory
 
-    # Iterate through the posts and download them
-    for post in profile.get_posts():
-        if downloaded_count >= 5:
-            break  # Exit the loop if 5 posts have been downloaded
+# Change the current working directory to the save directory
+os.chdir(save_dir)
+
+# Counter to track the number of downloaded posts
+downloaded_count = 0
+
+# Iterate through the posts and download images and videos
+for post in profile.get_posts():
+    if downloaded_count >= 5:
+        break  # Exit the loop if 5 posts have been downloaded
+    
+    # Check if the post is an image or video based on file extension
+    if post.is_video:
+        # Download the video
+        L.download_post(post, PROFILE)
+        print(f"Post {post.shortcode} saved at {post.url}")
+        downloaded_count += 1
+    elif post.typename == "GraphImage" and (post.url.endswith('.jpg') or post.url.endswith('.jpeg')):
+        # Download the image
         L.download_post(post, PROFILE)
         print(f"Post {post.shortcode} saved at {post.url}")
         downloaded_count += 1
 
-    # Schedule the function to run every hour
-    schedule.every().hour.do(download_posts)
-    
-    # Infinite loop to keep the script running
-    while True:
-    schedule.run_pending()
-    time.sleep(1) # Sleeping for one second
--- copyMedia.sh
+++ copyMedia.sh
@@ -7,4 +7,4 @@ downloadedMediaDir="/Users/ri/nba" # Downloaded Media
 projectDir="/Users/ri/Documents/NBAStatsPlus/NBAStatsPlus/NBAStatsPlus" # Xcode Project Directory
 
 # Copy downloaded media files to your Xcode project directory
-cp -R "$downloadedMediaDir"/* "$projectDir/Assets.xcassets/Videos"
+cp -R "$downloadedMediaDir"/* "$projectDir/DownloadedMedia"
